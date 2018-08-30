7e7cb8d4520baa9fd5394f2bf408df50bc84e508
Denilza Lima <denilzalimas@gmail.com>
2018-08-29 23:11:31 -0300

    criada class Page

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
+
+
+	$page = new Page();
+	$page->setTpl("index");
     
-	$sql= new Hcode\DB\Sql();
-	$results = $sql->select("SELECT *FROM tb_users");
-	echo json_encode($results);
+	//$sql= new Hcode\DB\Sql(); Testando o Banco de Dados
+	//$results = $sql->select("SELECT *FROM tb_users");
+	//echo json_encode($results);
 
 });
 
