353fbd84021df5b70ca5dc4cf633d7244e884565
Denilza Lima <denilzalimas@gmail.com>
2018-09-13 19:51:18 -0300

    Admin-Categorias

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -6,10 +6,25 @@ class Model {
 	{
 		$method = substr($name, 0, 3);
 		$fieldName = substr($name, 3, strlen($name));
-		//switch ($method);
-		var_dump($method,$fieldName);
-		exit;
-		
+		switch ($method)
+		{
+			case "get":
+				return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
+			break;
+			case "set":
+				$this->values[$fieldName] = $args[0];
+			break;
+		}
+	}
+	public function setData($data = array())
+	{
+		foreach ($data as $key => $value) {
+			
+			$this->{"set".$key}($value);
+		}
+	}
+	public function getValues()
+	{
+		return $this->values;
 	}
 }
- ?>
